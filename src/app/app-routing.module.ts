562932942f68263ba0348920b74e3d5ea1136c1f
Mika Rebensburg <mika@rebensburg.net>
2021-02-23 17:07:56 +0100

    basic navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { WeatherComponent } from './weather/weather.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home-component', component: HomeComponent },
+  { path: 'weather-component', component: WeatherComponent },
+  { path: 'search-component', component: SearchComponent },
+  { path: 'about-component', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
