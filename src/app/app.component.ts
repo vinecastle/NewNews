d2515016e44099f358a493120c63d3f265eecc0c
Ika J. Injac <ika.injac@gmail.com>
2021-03-01 14:37:46 +0100

    Revert "refactoring App.articles to App.storedArticles"
    This reverts commit ef210ac950d97c7355a18afe2333bb4296a90443.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,10 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
   title = 'NewNews';
-  storedArticles: Observable<any[]>;
+  articles: Observable<any[]>;
   comments: Observable<any[]>;
   constructor(private router: Router, firestore: AngularFirestore){
-    this.storedArticles = firestore.collection('articles').valueChanges();
+    this.articles = firestore.collection('articles').valueChanges();
     this.comments = firestore.collection('comments').valueChanges();
   }
 
--- src/app/comment.ts
+++ src/app/comment.ts
@@ -1,12 +1,9 @@
-
-
 export class Comment {
-    commentId: number;//Ought to be int?
-    articleId: number;//Ought to be int?
+    commentId: string;//Ought to be int?
+    articleId: string;//Ought to be int?
     author: {
         name: string;
         email: string
     }
     content: string;
-    time: string;//Ought to be timestamp
 }
