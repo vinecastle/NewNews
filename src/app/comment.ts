6a9460c3db8de1385cd8c711246e614b9b5073c3
Michaela <mi4300lj-s@student.lu.se>
2021-03-03 17:02:44 +0100

    Minor edit

--- src/app/comment.ts
+++ src/app/comment.ts
@@ -1,6 +1,6 @@
 export class Comment {
     commentId: string;//Ought to be int?
-    articleId: string;//Ought to be int?
+    articleURL: string;//Ought to be int?
     author: {
         name: string;
         email: string
