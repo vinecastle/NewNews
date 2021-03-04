738048aa8c40642a103e15202cc13877055842be
Michaela <mi4300lj-s@student.lu.se>
2021-03-04 14:57:03 +0100

    Contact form starting point
    Added graphic elements and error messages to help the user fill in the form correctly.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  email = new FormControl('', [Validators.required, Validators.email]);
+  messageFC = new FormControl('', [Validators.required, Validators.maxLength(30)]); 
+  name = new FormControl('', [Validators.required]);
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  //also figure out how custom validators work, no error message now if there's no name
+  getEmptyMessageError() {
+    if(this.messageFC.hasError('required')) {
+      return "Please write a message."; 
+    }
+  }
+
+  getNoNameError() {
+    if(this.name.hasError('required')) {
+      return "Please write your name."; 
+    }
+  }
+
+  getEmailErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter an email.';
+    }
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+  // handle other error messages, if input in email does not have the correct structure?
 
 }
