89a30cd846962d2a265458a74a2b6eff618d8291
Ibrahim Qatato <ibrahimq1998@gmail.com>
2021-03-07 15:54:09 +0100

    Title now shown on browser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router'; 
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,7 @@ export class AppComponent {
   title = 'NewNews';
   articles: Observable<any[]>;
   comments: Observable<any[]>;
-  constructor(private router: Router, firestore: AngularFirestore){
+  constructor(private router: Router, firestore: AngularFirestore, private titleService: Title){
     this.articles = firestore.collection('articles').valueChanges();
     this.comments = firestore.collection('comments').valueChanges();
   }
@@ -20,5 +21,8 @@ export class AppComponent {
   // todo, load home
   goToArticle(articleNo) {
     this.router.navigate(["article-component"], {queryParams: {article: articleNo}});
+  }
+  ngOnInit(){
+    this.titleService.setTitle(this.title);
   }
 }
